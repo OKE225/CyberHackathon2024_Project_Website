b990673ef06e56f9df8ba93faa0e4529cccc9959
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-05-05 11:06:23 +0200

    Add responsive Navbar component with styles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import "./navbar.scss";
+import powerpoint from "./icons/powerpoint.png";
+import github from "./icons/github.png";
+
+export default class Navbar extends Component {
+  state = {
+    active: false,
+  };
+
+  toggle = () => {
+    this.setState({ active: !this.state.active });
+    // window.addEventListener("resize", function() {
+    //   if (this.state.active && window.innerWidth < 1024) {
+    //     document.querySelector("nav").style.height = "calc(50px + 0.5vh)";
+    //   } else {
+    //     document.querySelector("nav").style.height = "98vh";
+    //   }
+    // }
+    if (this.state.active) {
+      document.querySelector("nav").style.height = "calc(50px + 0.5vh)";
+    } else {
+      document.querySelector("nav").style.height = "98vh";
+    }
+
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 1024 && this.state.active) {
+        document.querySelector("nav").style.height = "calc(50px + 0.5vh)";
+      } else {
+        this.setState({ active: this.state.active });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <nav>
+        <div className={`container ${this.state.active ? "active" : ""}`}>
+          <div className="icons">
+            <a href="">
+              <img src={powerpoint} alt="powerpoint icon" />
+            </a>
+            <a href="">
+              <img src={github} alt="github icon" />
+            </a>
+          </div>
+
+          <ul className="nav-list">
+            <li className="nav-link">
+              <a href="#" class="nav-item">
+                Ochrona danych
+              </a>
+            </li>
+            <li className="nav-link">
+              <a href="#" class="nav-item">
+                Autoryzacja
+              </a>
+            </li>
+            <li className="nav-link">
+              <a href="#" class="nav-item">
+                Nasza aplikacja
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="toggle">
+          <span
+            className="icon"
+            onClick={this.toggle}
+            onChange={this.toggle}></span>
+        </div>
+      </nav>
+    );
+  }
+}
